b1c2de02a9e726c6c890c6644f5e1e5a731a1fed
Micah Alpern <malpern@alpern.org>
2024-11-28 14:39:19 -0800

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Micah Alpern on 11/28/24.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
