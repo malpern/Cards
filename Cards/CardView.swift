a18ed86847c0dbb33a6c218cc5b6b175ea0c8eeb
Micah Alpern <malpern@alpern.org>
2024-11-29 07:17:04 -0800

    made basic card, gonna work on the back/flip

---
+++ Cards/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Cards
+//
+//  Created by Micah Alpern on 11/29/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
--- Cards/ContentView.swift
+++ Cards/ContentView.swift
@@ -8,17 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    var text: String = "Hello, World!"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Text(text)
+                .font(.system(size: 24))
+                .bold()
+                .multilineTextAlignment(.leading)
+            Image(systemName: "arrow.left.arrow.right.circle.fill")
+                .font(.system(size: 36))
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
         }
         .padding()
+        .frame(width: 300, height: 225)
+        .background(Color.orange)
+        .clipShape(.rect(cornerRadius: 10))
+        .shadow(radius: 10, x: 0, y: 10)
+        .padding()
+
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(text: "What is 7+7?")
+}
+#Preview {
+    ContentView(text: "What is the airspeed velocity of an unladen swallow?")
 }
